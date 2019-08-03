1cefd0381571cea58edc5ce4933562854032f2b9
Sheheryar Parvaz <skipper308@hotmail.ca>
2019-08-03 11:22:39 -0400

    Add basic tab navigation bar

--- App.js
+++ App.js
@@ -1,114 +1,62 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from "react";
+import MaterialIcon from "react-native-vector-icons/MaterialIcons";
+import SimpleLineIcon from "react-native-vector-icons/SimpleLineIcons";
+import { View, Text, StyleSheet } from "react-native";
+import { createMaterialTopTabNavigator, createAppContainer } from "react-navigation";
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
+const Placeholder = () => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text>UNIMPLEMENTED</Text>
+  </View>
+);
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  label: {
+    fontSize: 10,
   },
 });
 
-export default App;
+const icons = {
+  "Posts": {
+    mod: MaterialIcon,
+    name: "chat",
+  },
+  "Form": {
+    mod: SimpleLineIcon,
+    name: "note",
+  },
+  "Congress": {
+    mod: MaterialIcon,
+    name: "people",
+  },
+  "Settings": {
+    mod: MaterialIcon,
+    name: "settings",
+  },
+};
+
+const TabNavigator = createMaterialTopTabNavigator(
+  {
+    Posts: Placeholder,
+    Form: Placeholder,
+    Congress: Placeholder,
+    Settings: Placeholder,
+  },
+  {
+    initialRouteName: "Posts",
+    swipeEnabled: true,
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+      showIcon: true,
+      labelStyle: styles.label,
+    },
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { name, mod: Icon } = icons[navigation.state.routeName];
+        return <Icon name={name} color={tintColor} size={26} />;
+      },
+    }),
+  }
+);
+
+export default createAppContainer(TabNavigator);

7408a8fe331df8340c364f7ff05164d59f6eb179
The Floating String <laurencel2001@gmail.com>
2019-08-07 23:02:10 -0400

    Add react-native-elements deps and Posts scrollbar

--- App.js
+++ App.js
@@ -1,8 +1,46 @@
 import React from "react";
 import MaterialIcon from "react-native-vector-icons/MaterialIcons";
 import SimpleLineIcon from "react-native-vector-icons/SimpleLineIcons";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView} from "react-native";
 import { createMaterialTopTabNavigator, createAppContainer } from "react-navigation";
+import { Card, ListItem, Button, Icon } from 'react-native-elements'
+
+
+
+const PostsPlaceholder = () => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <ScrollView>
+      <Text style={{justifyContent: "center", alignItems: "center", fontSize: 50, margin: 20}}>Posts</Text>
+      <NewsCard title="MariMeet is Here" 
+      imageuri="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Coll%C3%A8ge_Marianopolis.JPG/1920px-Coll%C3%A8ge_Marianopolis.JPG"
+      text="MariMeet is a friendly afternoon to meet new students!" />
+      <NewsCard title="Used Books Sale" 
+      imageuri="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Coll%C3%A8ge_Marianopolis.JPG/1920px-Coll%C3%A8ge_Marianopolis.JPG"
+      text="Get second hand textbooks at a convenient price." />
+    </ScrollView>
+  </View>
+);
+
+class NewsCard extends React.Component {
+  render() {
+    return (
+
+    <Card
+      title={this.props.title}
+      image={{uri: this.props.imageuri}}>
+      <Text style={{marginBottom: 10}}>
+        {this.props.text}
+      </Text>
+      <Button
+        icon={<Icon name='code' color='#ffffff' />}
+        backgroundColor='#03A9F4'
+        buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+        title='VIEW NOW' />
+    </Card>
+
+    )
+  }
+}
 
 const Placeholder = () => (
   <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -37,7 +75,7 @@ const icons = {
 
 const TabNavigator = createMaterialTopTabNavigator(
   {
-    Posts: Placeholder,
+    Posts: PostsPlaceholder,
     Form: Placeholder,
     Congress: Placeholder,
     Settings: Placeholder,

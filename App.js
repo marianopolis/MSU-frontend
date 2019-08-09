e38208af452bd923cce4a8e30784110a67f8d1b5
The Floating String <laurencel2001@gmail.com>
2019-08-08 23:54:14 -0400

    Add minimal static layout for the mobile app

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import MaterialIcon from "react-native-vector-icons/MaterialIcons";
 import SimpleLineIcon from "react-native-vector-icons/SimpleLineIcons";
-import { View, Text, StyleSheet, Image, ScrollView} from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView, FlatList} from "react-native";
 import { createMaterialTopTabNavigator, createAppContainer } from "react-navigation";
-import { Card, ListItem, Button, Icon } from 'react-native-elements'
+import { Header, Card, ListItem, Button, Icon, Input, Avatar } from 'react-native-elements'
 
 const PostsPlaceholder = () => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "stretch" }}>
+    <Header
+      centerComponent={{ text: 'Posts', style: { color: '#fff', fontSize: 20 }}}
+      />
     <ScrollView>
-      <Text style={{justifyContent: "center", alignItems: "center", fontSize: 50, margin: 20}}>Posts</Text>
       <NewsCard title="MariMeet is Here" 
       imageuri="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Coll%C3%A8ge_Marianopolis.JPG/1920px-Coll%C3%A8ge_Marianopolis.JPG"
       text="MariMeet is a friendly afternoon to meet new students!" />
@@ -18,6 +20,61 @@ const PostsPlaceholder = () => (
     </ScrollView>
   </View>
 );
+
+const FormPlaceholder = () => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "stretch" }}>
+    <Header
+      centerComponent={{ text: 'Form', style: { color: '#fff', fontSize: 20 }}}
+      />
+    <ScrollView>
+      <FormCard />
+    </ScrollView>
+  </View>
+);
+
+const CongressPlaceholder = () => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "stretch" }}>
+    <Header
+      centerComponent={{ text: 'Congress', style: { color: '#fff', fontSize: 20 }}}
+      />
+    <ScrollView>
+      <MemberCard title="Aria Khiabani" 
+      imageuri="http://msucongress.com/wp-content/uploads/2019/07/Aria-800x1068.jpg"
+      text="President" />
+      <MemberCard title="Ying Ge" 
+      imageuri="http://msucongress.com/wp-content/uploads/2019/07/YG-800x1068.jpg"
+      text="Vice President" />
+      <MemberCard title="Amanda Morrone" 
+      imageuri="http://msucongress.com/wp-content/uploads/2019/07/Amanda-800x1068.jpg"
+      text="Vice President of Finance" />
+      <MemberCard title="Ying Chen" 
+      imageuri="http://msucongress.com/wp-content/uploads/2019/07/YC-800x1068.jpg"
+      text="Coordinator of Communications" />
+      <MemberCard title="Michael Chalkhoun" 
+      imageuri="http://msucongress.com/wp-content/uploads/2019/07/Michael-800x1068.jpg"
+      text="Coordinator of Student Advocacy" /> 
+      <MemberCard title="Darya Jabbari" 
+      imageuri="http://msucongress.com/wp-content/uploads/2019/07/Darya-800x1068.jpg"
+      text="Coordinator of Social Justice" />
+      <MemberCard title="Andrew Petrecca-Berthelet" 
+      imageuri="http://msucongress.com/wp-content/uploads/2019/07/Andrew-800x1068.jpg"
+      text="Coordinator of Social Activities" />
+    </ScrollView>
+  </View>
+);
+
+const SettingsPlaceholder = () => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "stretch" }}>
+    <Header
+      centerComponent={{ text: 'Settings', style: { color: '#fff', fontSize: 20 }}}
+      />
+    <ScrollView style={{padding: 20}}>
+    <Text>About this app</Text>
+    <Text>Created in August 2019 by the MSU Tech Committee</Text>
+    </ScrollView>
+  </View>
+);
+
 
 class NewsCard extends React.Component {
   render() {
@@ -29,10 +86,50 @@ class NewsCard extends React.Component {
         {this.props.text}
       </Text>
       <Button
-        icon={<Icon name='code' color='#ffffff' />}
+        icon={<Icon name='fingerprint' color='#ffffff' />}
         backgroundColor='#03A9F4'
         buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-        title='VIEW NOW' />
+        title='View now' />
+    </Card>
+    )
+  }
+}
+
+class FormCard extends React.Component {
+  render() {
+
+    return(
+    <Card>
+      <Text> Have your say! </Text>
+      <Input multiline={true} placeholder="Your message" />
+      <Button
+        icon={<Icon name='send' color='#ffffff' />}
+        backgroundColor='#03A9F4'
+        buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+        title='Send' />
+    </Card>
+    )
+  }
+}
+
+class MemberCard extends React.Component {
+  render() {
+    return (
+    <Card
+      title={this.props.title}
+      >
+      <Image
+          style={{width: 50, height: 50}}
+          source={{uri: this.props.imageuri}}
+        />
+      <Text style={{marginBottom: 10}}>
+        {this.props.text}
+      </Text>
+      <Button
+        icon={<Icon name='chat' color='#ffffff' />}
+        backgroundColor='#03A9F4'
+        buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+        title='Contact' />
     </Card>
     )
   }
@@ -72,9 +169,9 @@ const icons = {
 const TabNavigator = createMaterialTopTabNavigator(
   {
     Posts: PostsPlaceholder,
-    Form: Placeholder,
-    Congress: Placeholder,
-    Settings: Placeholder,
+    Form: FormPlaceholder,
+    Congress: CongressPlaceholder,
+    Settings: SettingsPlaceholder,
   },
   {
     initialRouteName: "Posts",

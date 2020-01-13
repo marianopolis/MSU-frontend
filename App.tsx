db416447201439e0df9c1e97f25d750221f23ebb
thomasjirafe <thomasjirafe@gmail.com>
2020-01-13 11:43:19 -0500

    Remove hardcoded congress placeholder because congressmembers are now fetched from backend instead

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import PostsScreen from "./src/PostsScreen";
 import FilesScreen from "./src/FilesScreen";
 /* import FormsScreen from "./src/FormsScreen"; */
 import EventsScreen from "./src/EventsScreen";
+import CongressMembersScreen from "./src/CongressMembersScreen"
 
 const MemberCard = ({
   imageuri,
@@ -28,78 +29,6 @@ const MemberCard = ({
     subtitleStyle={{ color: "black" }}
     subtitle={text}
   />
-);
-
-const CongressPlaceholder = () => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "stretch" }}>
-    <ScrollView>
-      <MemberCard
-        title="Aria Khiabani"
-        imageuri="./static/photos/Aria.jpg"
-        text="President"
-      />
-      <MemberCard
-        title="Ying Ge"
-        imageuri="./static/photos/YingG.jpg"
-        text="Vice President"
-      />
-      <MemberCard
-        title="Amanda Morrone"
-        imageuri="./static/photos/Amanda.jpg"
-        text="Vice President of Finance"
-      />
-      <MemberCard
-        title="Ying Chen"
-        imageuri="./static/photos/YingC.jpg"
-        text="Coordinator of Communications"
-      />
-      <MemberCard
-        title="Michael Chalkhoun"
-        imageuri="./static/photos/Michael.jpg"
-        text="Coordinator of Student Advocacy"
-      />
-      <MemberCard
-        title="Darya Jabbari"
-        imageuri="./static/photos/Darya.jpg"
-        text="Coordinator of Social Justice"
-      />
-      <MemberCard
-        title="Andrew Petrecca-Berthelet"
-        imageuri="./static/photos/Andrew.jpg"
-        text="Coordinator of Social Activities"
-      />
-      <MemberCard
-        title="Cherry Ying"
-        imageuri="./static/photos/Cherry.jpg"
-        text="Coordinator Volunteering & Activities"
-      />
-      <MemberCard
-        title="Misha Fotovati"
-        imageuri="./static/photos/Misha.jpg"
-        text="Coordinator of Internal Affairs"
-      />
-      <MemberCard
-        title="Zhaoran Wu"
-        imageuri="./static/photos/Zhaoran.jpg"
-        text="Coordinator of Cultural Affairs"
-      />
-      <MemberCard
-        title="Steven Gong"
-        imageuri="./static/photos/Steven.jpg"
-        text="Financial Assistant"
-      />
-      <MemberCard
-        title="Stephanie Wang"
-        imageuri="./static/photos/Stephanie.jpg"
-        text="Administrative Assistant"
-      />
-      <MemberCard
-        title="Laurence Liang"
-        imageuri="./static/photos/Laurence.jpg"
-        text="Coordinator of External Affairs"
-      />
-    </ScrollView>
-  </View>
 );
 
 const MSU_LOGO = require("./assets/logo-white.png");
@@ -147,7 +76,7 @@ const TabNavScreen = () => (
     <TabNav.Screen name="Posts" component={PostsScreen} />
     <TabNav.Screen name="Files" component={FilesScreen} />
     <TabNav.Screen name="Events" component={EventsScreen} />
-    <TabNav.Screen name="Congress" component={CongressPlaceholder} />
+    <TabNav.Screen name="Congress" component={CongressMembersScreen} />
   </TabNav.Navigator>
 );
 
